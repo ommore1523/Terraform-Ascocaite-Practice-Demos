fac6fc606c47d84972f4069b9c226300967e773f
omkar <omkar.more@expansivegroup.com>
2024-08-05 19:57:56 +0530

    Quetion added

--- demo15_local_values/locals.tf
+++ demo15_local_values/locals.tf
@@ -12,3 +12,15 @@ variable "project" {
 locals {
   deployment_name = "${var.env}-${var.env}"
 }
+
+# Quetions
+# ANS
+
+# variable "project" {
+#   type = string
+#   default = "lasso" <<-- Here we cannot use locals
+# }
+
+
+# In Terraform, the default value of a variable cannot reference other variables or 
+# local values. This restriction exists because the default value must be a constant expression.
